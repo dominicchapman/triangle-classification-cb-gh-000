5c08f62a03976641eda9024b1c27d74143db50a4
Dominic Chapman <dominicchapman@me.com>
2017-10-14 19:21:23 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,8 +2,13 @@ class Triangle
   # write code here
 
   attr_accessor :first, :second, :third
-  
+
   def initialize(first, second, third)
+    @first = first
+    @second = second
+    @third = third
   end
-  
+
+  def 
+
 end
